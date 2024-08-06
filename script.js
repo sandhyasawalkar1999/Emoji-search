84cb9ca3881fa76997b3c5f607d45c1164ee81c9
sandhyasawalkar1999 <sandhyasawalkar1999@gmail.com>
2024-08-06 14:47:26 +0530

    second commit

--- script.js
+++ script.js
@@ -1,5 +1,78 @@
 let emojee_container = document.getElementById("emojee_container");
 let serach_filed = document.getElementById("serach");
+let filterEmg =document.getElementById("filter-emojee");
+
+
+// filterEmg.addEventListener("click", (e) => {
+//   const button = e.target.closest(".filter-btn");
+  
+//   if (button) {
+//       e.preventDefault();
+//       const category = button.getAttribute("data-category");
+//       filterFunction(category);
+//   }
+// });
+
+// let filterFunction = (value) => {
+//   let filteredData;
+
+//   if (value.toLowerCase() === "all") {
+//       filteredData = emojiList;
+//   } else {
+//       filteredData = emojiList.filter(e => {
+//           if (e.description.toLowerCase().includes(value.toLowerCase())) {
+//               return true;
+//           }
+//           if (e.aliases.some(alias => alias.toLowerCase().startsWith(value.toLowerCase()))) {
+//               return true;
+//           }
+//           if (e.tags.some(tag => tag.toLowerCase().startsWith(value.toLowerCase()))) {
+//               return true;
+//           }
+//           return false;
+//       });
+//   }
+
+//   displayEmoji(filteredData);
+// };
+
+// filteraEmoji.addEventListener("click" ,(e) =>{
+//   const button = e.target.closest(".filter-btn");
+
+//   if(button){
+//     e.preventDefault();
+//     const category = button.getAttribute("data-category");
+//     filterFunction(category);
+//   }
+// });
+
+let face = document.getElementById("face").value;
+console.log(face);
+
+
+let filterFunction = (value) =>{
+  let filteredData = emojiList;
+  
+      if(value.toLowerCase() === "all"){
+        filteredData = emojiList;
+      }
+      else{
+        filteredData = emojiList.filter(e => {
+          if(e.description.toLowerCase().includes(value.toLowerCase())){
+            return true;
+          }
+          if(e.aliases.some(alias => alias.toLowerCase().startwith(value.toLoerCase()))){
+            return true;
+          }
+          if(e.tags.some(tag => tag.toLowerCase().startswith(value.toLoerCase()))){
+            return true;
+          }
+          return false;
+        });
+      }
+displayEmojee(filteredData());
+
+}
 
 function displayEmojee(serachQuery = "") {
   // console.log(emojiList);
@@ -10,9 +83,12 @@ function displayEmojee(serachQuery = "") {
     }
   });
 
+ 
+
   console.log(filteredList);
 
   // console.log(emojiList);
+  emojee_container.innerHTML = "";
   filteredList.forEach(function (emoji) {
     //   console.log(emoji);
 
@@ -24,7 +100,8 @@ function displayEmojee(serachQuery = "") {
 
     let new_descrption = document.createElement("td");
 
-    console.log(new_row, new_descrption, new_emoji, new_aliases);
+    // console.log(new_row, new_descrption, new_emoji, new_aliases);
+    console.log(new_emoji);
 
     new_descrption.innerText = emoji.description;
     new_emoji.innerText = emoji.emoji;
@@ -38,11 +115,20 @@ function displayEmojee(serachQuery = "") {
 
     // emoji_container.appendChild(new_row);
 
-    emojee_container.appendChild(new_row);
+    emojee_container.appendChild(new_emoji);
   });
 }
 
-window.addEventListener("load", displayEmojee);
+const allButton = document.getElementById("all")
+allButton.addEventListener("click", displayEmojee())
+window.addEventListener("load", displayEmojee());
+serach_filed.addEventListener("click",(e) =>
+{
+  let value = e.target.value;
+  console.log(value);
+  faceDisplay(value);
+
+});
 serach_filed.addEventListener("keyup", (e) => {
   let value = e.target.value;
   console.log(value);
